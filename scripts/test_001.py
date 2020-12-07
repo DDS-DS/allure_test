4205a391e7f38c2a3328cffb7fc9787ed1f5bb90
DDS <1296470317@qq.com>
2020-12-08 02:05:02 +0800

    12

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -1,17 +1,18 @@
-# 时间 2020/12/7 21:02
-import pytest,allure
+import pytest
+import allure
 
-class Test_Abc:
+
+class Test_Abc():
+
     @allure.step(title='第一个测试')
-    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    # @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
     def test_001(self):
         allure.attach("这是一个描述","试一下")
         assert 1
 
     @allure.issue('http://www.baidu.com')
-    @pytest.allure.testcase("http://www.baidu.com/test_001")
-    @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
+    # @pytest.allure.testcase("http://www.baidu.com/test_001")
+    # @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
     def test_002(self):
         allure.attach("这是第二个描述",'....')
         assert 0
-
