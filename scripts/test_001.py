6399b58a6d7f7fb13215eb77c001a1c38c50ff12
DDS <1296470317@qq.com>
2020-12-08 02:47:18 +0800

    12

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -5,14 +5,16 @@ import allure
 class Test_Abc():
 
     @allure.step(title='第一个测试')
-    # @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
+    @allure.severity(allure.severity_level.TRIVIAL)
     def test_001(self):
         allure.attach("这是一个描述","试一下")
         assert 1
 
     @allure.issue('http://www.baidu.com')
-    # @pytest.allure.testcase("http://www.baidu.com/test_001")
-    # @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
+    @allure.testcase("http://www.baidu.com/test_001")
+    @allure.severity(allure.severity_level.TRIVIAL)
     def test_002(self):
         allure.attach("这是第二个描述",'....')
         assert 0
+if __name__ == '__main__':
+    pytest
